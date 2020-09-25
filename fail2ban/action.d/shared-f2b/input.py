16748ab546425c8dcf680dcc82f3dfbc810e4009
Adam Boutcher <adam.j.boutcher@durham.ac.uk>
2020-09-25 16:36:13 +0100

    Updated input script for Python3 and the new API.

--- fail2ban/action.d/shared-f2b/input.py
+++ fail2ban/action.d/shared-f2b/input.py
@@ -8,25 +8,61 @@ import mysql.connector
 import datetime
 import socket
 
-print sys.argv[0] #scritpt name
-print sys.argv[1] #jailename
-print sys.argv[2] #protocol
-print sys.argv[3] #port
-print sys.argv[4] #ip
-print sys.argv[5] #bantime
+import shared_cfg as cfg
+
+print (sys.argv[0]) #scritpt name
+print (sys.argv[1]) #jailname
+print (sys.argv[2]) #protocol
+print (sys.argv[3]) #port
+print (sys.argv[4]) #ip
+print (sys.argv[5]) #bantime
 
 date1 = datetime.datetime.now()
 
-faildb = mysql.connector.connect(
-        host='127.0.01',
-        user='username',
-        passwd='password1',
-        database='f2b')
+if 'mysql' in cfg.source:
+    db = mysql.connector.connect(host=cfg.mysql["host"], user=cfg.mysql["user"], passwd=cfg.mysql["passwd"], db=cfg.mysql["db"])
+    faildb = mysql.connector.connect(
+            host='127.0.01',
+            user='username',
+            passwd='password1',
+            database='f2b')
 
-con1 = faildb.cursor()
-sql = "INSERT INTO f2b SET hostname=\'" + socket.gethostname() + "\', created=\'" + str(date1) + "\', jail=\'" + sys.argv[1] + "\', protocol=\'" + sys.argv[2] + "\', port=\'" + sys.argv[3] + "\', ip=\'" + sys.argv[4] + "\', bantime=\'" + sys.argv[5] + "\';"
-con1.execute(sql)
-faildb.commit()
+    cur = db.cursor()
+    sql = "INSERT INTO f2b SET hostname=\'" + socket.gethostname() + "\', created=\'" + str(date1) + "\', jail=\'" + sys.argv[1] + "\', protocol=\'" + sys.argv[2] + "\', port=\'" + sys.argv[3] + "\', ip=\'" + sys.argv[4] + "\', bantime=\'" + sys.argv[5] + "\';"
+    cur.execute(sql)
+    db.commit()
+else:
+    import json
+    import time
+    data= {
+        "hostname": str(socket.gethostname()),
+        "created": int(time.time()),
+        "jail": str(sys.argv[1]),
+        "protocol": str(sys.argv[2]),
+        "port": str(sys.argv[3]),
+        "ip": str(sys.argv[4]),
+        "bantime": str(sys.argv[5])
+    }
+    data = json.dumps(data)
+    if (sys.version_info > (3, 0)):
+        # Python 3 code in this block
+        import urllib.request
+        req = urllib.request.Request(cfg.apiurl+"/put", data=bytes(data.encode("utf-8")), method='PUT')
+        req.add_header("Content-type", "application/json; charset=UTF-8")
+        req.add_header("X-TOKEN", cfg.apitoken)
+        resp = urllib.request.urlopen(req)
+        resp = resp.read().decode('utf-8')
+    else:
+        # Python 2 code in this block
+        import urllib
+        import urllib2
+        opener = urllib2.build_opener(urllib2.HTTPHandler)
+        req = urllib2.Request(cfg.apiurl+"/put", data=bytes(data.encode("utf-8")))
+        req.get_method = lambda: 'PUT'
+        req.add_header("Content-type", "application/json; charset=UTF-8")
+        req.add_header("X-TOKEN", cfg.apitoken)
+        resp = opener.open(req)
+        resp = resp.read()
+    print("API Returned: "+resp)
 
-print "IP added to database"
-
+print ("IP added to database")
