d12249f1788719ed8397148a686804b3a4522eb2
Adam Boutcher <adam.j.boutcher@durham.ac.uk>
2021-02-10 12:10:48 +0000

    Tidied up the get.py

--- fail2ban/action.d/shared-f2b/get.py
+++ fail2ban/action.d/shared-f2b/get.py
@@ -1,17 +1,19 @@
 #!/usr/bin/fail2ban-python
 # Shared Fail2Ban
-# modified Feb 2021 Jon Trinder  to add command line arg
-# as of 01/02/2021 I hadnt tested the 'mysql' route through here so may have broken the query
-# Paul Clark, Adam Boutcher
+# This is the script that pulls Banned IPs in a given JailName and output it to a file.
 # github.com/bulgemonkey/Shared-Fail2Ban/
-
+#
+# Paul Clark    - 2018, IPPP, Durham University
+#  Initial Database work
+# Adam Boutcher - 2020, IPPP, Durham University
+#  Inital API work
+# Jon Trinder   - 2021, Glasgow University
+#  Added arguments to support multiple jailnames (MySQL Untested), default to ssh to keep compatability.
 
 import sys
 import json
 import argparse
-
 import shared_cfg as cfg
-
 
 def getjails(jailname):
 
@@ -26,19 +28,17 @@ def getjails(jailname):
         cur.execute(sql)
         data = cur.fetchall()
     else:
-        queryline = cfg.apiurl + "/time/" + jailname + "/1"
+        queryline =
         if (sys.version_info > (3, 0)):
             # Python 3 code in this block
             import urllib.request
-            #response = urllib.request.urlopen(cfg.apiurl+"/time/ssh/1")
-            response = urllib.request.urlopen(queryline)
+            response = urllib.request.urlopen(cfg.apiurl+"/time/"+jailname+"/1")
         else:
             # Python 2 code in this block
             import urllib
-            #response = urllib.urlopen(cfg.apiurl+"/time/ssh/1")
-            response = urllib.urlopen(queryline)
+            response = urllib.urlopen(cfg.apiurl+"/time/"+jailname+"/1")
         data = json.loads(response.read())
-    logfilename = "/etc/fail2ban/action.d/shared-f2b/filter-" + jailname + ".log"
+    logfilename = "/etc/fail2ban/action.d/shared-f2b/filter-"+jailname+".log"
 
     open(logfilename, "w").close()
     file = open(logfilename, "w")
@@ -50,15 +50,7 @@ def getjails(jailname):
 
 if __name__ == '__main__':
 
-    """
-    jjt added args parse so I can pass in jailname    
-    """
     parser = argparse.ArgumentParser()
-    parser.add_argument('--jail', required=False,
-                        help='Enter jailname', dest='jail_name', default="ssh")
-
+    parser.add_argument('--jail', required=False, help='Enter jailname', dest='jail_name', default="ssh")
     results = parser.parse_args()
     getjails(results.jail_name)
-    #print(results.jail_name)   #left around for debugging to save me eretyping
-
-
